0b937d0344f0ecfa547a119e5bf419dcb266db11
MUKILTEO\1721807 <atkishun000@edmonds15.org>
2024-10-03 13:35:54 -0700

    ok

--- Text Adventure/Program.cs
+++ Text Adventure/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace Text_Adventure
+internal class Program
 {
-    internal class Program
+    private static void Main(string[] args)
     {
-        static void Main(string[] args)
+        Console.WriteLine("Welcome to Hunter's World in a chill room");
+        Console.WriteLine("What room do you like to go in");
+        Console.WriteLine("VGD | ANI | CSN | EET");
+
+        string input = Console.ReadLine();
+        if (input == "CSN")
         {
-            Console.WriteLine("Hello, World!");
+        }
         }
     }
+{}
 }
